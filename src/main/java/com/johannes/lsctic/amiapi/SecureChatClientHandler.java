6f9e80f42b980f2e0635fabc48dd0b2d1a2659c6
Johannes <engler.johannes@posteo.de>
2017-01-04 00:38:32 +0100

    Netty first steps

---
+++ src/main/java/com/johannes/lsctic/amiapi/SecureChatClientHandler.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.johannes.lsctic.amiapi;
+
+/**
+ *
+ * @author Johannes
+ */
+import com.johannes.lsctic.FXMLController;
+import com.johannes.lsctic.fields.HistoryField;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.logging.Logger;
+
+/**
+ * Handles a client-side channel.
+ */
+public class SecureChatClientHandler extends SimpleChannelInboundHandler<String> {
+    private final FXMLController fxml;
+    public SecureChatClientHandler(FXMLController fxml) {
+        this.fxml = fxml;
+    }
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.err.println(msg);
+       
+
+                boolean notEndedYet = true;
+                while (notEndedYet) {
+                    //This will wait until a line of text has been sent
+                    String chatInput = msg;
+                    switch (chatInput) {
+                        case "logoff":
+                            notEndedYet = false;
+                           
+                            break;
+                        case "success":
+                            System.out.println("Erfolgreich Verbunden");
+                            break;
+                        default:
+                            int op = Integer.valueOf(chatInput.substring(0, 3));
+                            String param = chatInput.substring(3,chatInput.length());
+                            Logger.getLogger(getClass().getName()).info(chatInput);
+                            switch(op) {
+                                case 0: {
+                                    String[] d = param.split(":");
+                                    String intern = d[0];
+                                    int status = Integer.valueOf(d[1]);
+                                    internNumbers.get(intern).setStatus(status);
+                                    break;
+                                }
+                                case 10: {
+                                    String[] d = param.split(":");
+                                    String source = d[0];
+                                    String destinatnion = d[1];
+                                    Date startTime = new Date(Long.parseLong(d[2]));
+                                    Long duration = Long.parseLong(d[3]);
+                                    int disposition = Integer.valueOf(d[4]);
+                                    System.out.println("CDR: von: "+source+" nach: "+destinatnion+" am "+startTime.toString()+" dauer "+ duration+" dispo: "+disposition);
+                                    if(source.equals(fxml.getOwnExtension()+"")){
+                                        fxml.addCdrAndUpdate(new HistoryField(destinatnion, startTime.toString(), duration.toString(), true));
+                                    } else {
+                                        fxml.addCdrAndUpdate(new HistoryField(destinatnion, startTime.toString(), duration.toString(), false));
+                                    }
+                                }
+                                default:
+                                    System.out.println("i liegt nicht zwischen null und drei");
+                            }   break;
+                    }
+                }
+            
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+    
+    
+    
+}
--- src/main/java/com/johannes/lsctic/amiapi/ServerConnectionHandler.java
+++ src/main/java/com/johannes/lsctic/amiapi/ServerConnectionHandler.java
@@ -6,14 +6,17 @@
 package com.johannes.lsctic.amiapi;
 
 import com.johannes.lsctic.FXMLController;
-import com.johannes.lsctic.fields.HistoryField;
 import com.johannes.lsctic.fields.InternField;
-import java.io.BufferedReader;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Date;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,103 +26,45 @@ import java.util.logging.Logger;
  * @author johannesengler
  */
 public class ServerConnectionHandler {
-    private final String adresse = "localhost";
-    private final int port = 12350;
-    private final Socket s;
-    private ClientThread t;
+     static final String ADRESSE = "localhost";
+     static final int PORT = 12350;
+    
+    private Channel ch;
     private final Map<String, InternField> internNumbers;
-    private FXMLController fcont;
+    private final FXMLController fcont;
     public ServerConnectionHandler(Map<String, InternField> internNumbers, FXMLController fcont) throws IOException {
-        s = new Socket(adresse, port); 
-         t = new ClientThread(s);
-        new Thread(t).start();
-        this.internNumbers = internNumbers;
-        this.fcont = fcont;
+         try {
+             final SslContext sslCtx = SslContextBuilder.forClient()
+                     .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+             
+             EventLoopGroup group = new NioEventLoopGroup();
+             
+             Bootstrap b = new Bootstrap();
+             b.group(group)
+                     .channel(NioSocketChannel.class)
+                     .handler(new SecureChatClientInitializer(sslCtx,  fcont));
+             
+             // Start the connection attempt.
+             ch = b.connect(ADRESSE, PORT).sync().channel();
+             
+             // Read commands from the stdin.
+             
+             
+             // If user typed the 'bye' command, wait until the server closes
+             // the connection.
+             
+             // Wait until all messages are flushed before closing the channel.
+             
+             
+             
+         } catch (InterruptedException ex) {
+             Logger.getLogger(ServerConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         this.internNumbers = internNumbers;
+             this.fcont = fcont;
     }
     public void sendBack(String msg) {
-        t.sendBack(msg);
+         ChannelFuture lastWriteFuture = ch.writeAndFlush(msg + "\r\n");
     }
     
-    class ClientThread implements Runnable
-    {
-        Socket threadSocket;
-        PrintWriter output;
-         
-        //This constructor will be passed the socket
-        public ClientThread(Socket socket)
-        {
-            //Here we set the socket to a local variable so we can use it later
-            threadSocket = socket;
-        }
-         
-        @Override
-        public void run()
-        {
-            //All this should look familiar
-            try {
-                //Create the streams
-               
-                sendBack("000201");
-                //Tell the client that he/she has connected
-                BufferedReader input = new BufferedReader(new InputStreamReader(threadSocket.getInputStream()));
-
-                boolean notEndedYet = true;
-                while (notEndedYet) {
-                    //This will wait until a line of text has been sent
-                    String chatInput = input.readLine();
-                    switch (chatInput) {
-                        case "logoff":
-                            notEndedYet = false;
-                            output.println("success");
-                            break;
-                        case "success":
-                            System.out.println("Erfolgreich Verbunden");
-                            break;
-                        default:
-                            int op = Integer.valueOf(chatInput.substring(0, 3));
-                            String param = chatInput.substring(3,chatInput.length());
-                            Logger.getLogger(getClass().getName()).info(chatInput);
-                            switch(op) {
-                                case 0: {
-                                    String[] d = param.split(":");
-                                    String intern = d[0];
-                                    int status = Integer.valueOf(d[1]);
-                                    internNumbers.get(intern).setStatus(status);
-                                    break;
-                                }
-                                case 10: {
-                                    String[] d = param.split(":");
-                                    String source = d[0];
-                                    String destinatnion = d[1];
-                                    Date startTime = new Date(Long.parseLong(d[2]));
-                                    Long duration = Long.parseLong(d[3]);
-                                    int disposition = Integer.valueOf(d[4]);
-                                    System.out.println("CDR: von: "+source+" nach: "+destinatnion+" am "+startTime.toString()+" dauer "+ duration+" dispo: "+disposition);
-                                    if(source.equals(fcont.getOwnExtension()+"")){
-                                        fcont.addCdrAndUpdate(new HistoryField(destinatnion, startTime.toString(), duration.toString(), true));
-                                    } else {
-                                        fcont.addCdrAndUpdate(new HistoryField(destinatnion, startTime.toString(), duration.toString(), false));
-                                    }
-                                }
-                                default:
-                                    System.out.println("i liegt nicht zwischen null und drei");
-                            }   break;
-                    }
-                }
-                threadSocket.close();
-            } catch(IOException exception) {
-                System.out.println("Error: " + exception);
-            }
-        }
-        private void sendBack(String msg) {
-            try {
-                System.out.println("abfrage geht raus: "+msg);
-                output = new PrintWriter(threadSocket.getOutputStream(), true);
-                output.println(msg);
-                output.flush();
-            } catch (IOException ex) {
-                Logger.getLogger(ServerConnectionHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
 }
