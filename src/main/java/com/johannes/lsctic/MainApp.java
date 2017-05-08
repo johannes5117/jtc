6ac769118d61da5372309a3682b590eb81e836af
Johannes Engler <johannes5117@gmail.com>
2017-05-09 01:11:32 +0200

    FX way has to be followed because swing is buggy

--- src/main/java/com/johannes/lsctic/MainApp.java
+++ src/main/java/com/johannes/lsctic/MainApp.java
@@ -13,14 +13,12 @@ import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import javafx.stage.WindowEvent;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
 
-import java.awt.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -39,7 +37,6 @@ public class MainApp extends Application implements NativeKeyListener {
 
     @Override
     public void start(Stage stage) throws Exception {
-        generateTray();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
         Parent root = loader.load();
         FXMLController controller = loader.getController();
@@ -47,13 +44,13 @@ public class MainApp extends Application implements NativeKeyListener {
         controller.startApp(eventBus);
         controller.setStage(stage);
         Scene scene = new Scene(root);
-        scene.setFill(null);
         Font.loadFont(MainApp.class.getResource("/styles/Roboto-Light.ttf").toExternalForm(),13);
-        //scene.getStylesheets().clear();
+
         scene.getStylesheets().add("/styles/Styles.css");
         stage.setTitle("JavaFX and Maven");
 
         stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
         stage.setScene(scene);
 
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
@@ -88,48 +85,6 @@ public class MainApp extends Application implements NativeKeyListener {
                 //TODO: Find way to securely shutdown program
             }
         });
-    }
-
-    private void generateTray() {
-        if (!SystemTray.isSupported()) {
-            System.out.println("SystemTray is not supported");
-            return;
-        }
-        final PopupMenu popup = new PopupMenu();
-        final TrayIcon trayIcon
-                = new TrayIcon(Toolkit.getDefaultToolkit().getImage("/pics/down.png"));
-        final SystemTray tray = SystemTray.getSystemTray();
-
-        // Create a pop-up menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
-        MenuItem exitItem = new MenuItem("Exit");
-
-        //Add components to pop-up menu
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
-        popup.add(exitItem);
-        trayIcon.setPopupMenu(popup);
-
-        try {
-            tray.add(trayIcon);
-        } catch (AWTException e) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, e);
-        }
     }
 
     /**
